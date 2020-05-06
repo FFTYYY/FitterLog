dcc14d178c91ba2fb444289d1e78aafefbcc8c98
YYY <1004473299@qq.com>
2020-05-06 17:55:15 +0800

    自动平滑

--- fitterlog_server_module/experiment/views/varaible_opt/__init__.py
+++ fitterlog_server_module/experiment/views/varaible_opt/__init__.py
@@ -1,3 +1,14 @@
+
+def smooth_lis(lis):
+	lim = 500
+	if len(lis) < lim:
+		return lis
+	p = len(lis) / lim #每p个采样一次
+
+	lis = [lis[int(p*i+0.5)] for i in range(lim) if int(p*i+0.5) < len(lis)]
+
+	return lis
+
 def get_tracks(track_lis):
 
 	t_vs = []
@@ -8,6 +19,7 @@ def get_tracks(track_lis):
 			if v.time_stamp < 0:
 				continue
 			vlis.append( (v.time_stamp , v.value) )
+		vlis = smooth_lis(vlis)
 		t_vs.append( (t , vlis))
 
 	return t_vs
