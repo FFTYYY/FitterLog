054b2f97ada90a1cdda8c560d66a1850a6b01070
YYY <1004473299@qq.com>
2020-05-09 22:15:35 +0800

    修复一个bug

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ print(pkgs)
 
 setup(
 	name 								= "fitterlog",
-	version 							= "0.1.10",
+	version 							= "0.1.11",
 	url 								= "http://github.com/FFTYYY/fitterlog",
 	description 						= "",
 	license 							= "MIT",
