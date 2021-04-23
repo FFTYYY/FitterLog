06f27c48726e91ebd781e98b165832b7db673e31
YYY <1004473299@qq.com>
2021-04-23 12:44:37 +0800

    增加了一点CUI示例

--- fitterclient/cmd_client.py
+++ fitterclient/cmd_client.py
@@ -3,6 +3,7 @@ from YTools.client.cui import CUI
 from YTools.universe.beautiful_str import merge_str
 from YTools.network.server.client import Requester
 from fitterlog.interface.restore import load_noun_number
+from base64 import b64encode , b64decode
 
 def noun_cnt(request):
 	return load_noun_number()
@@ -22,14 +23,32 @@ def processer_ask_syntax(ui , cmd):
 	syntax = ui.req.request("ask_syntax/{noun_id}".format(noun_id = noun_id))
 	ui.output(syntax.linearize())
 
+def processer_ask_val(ui , cmd):
+	noun_id = int(cmd[1])
+	pred_name = (" ".join(cmd[2:])).encode("utf-8")
+	pred_name = str( b64encode(  pred_name ) , encoding = "utf-8")
+	val = ui.req.request("ask_val/{noun_id}/{pred_name}".format(noun_id = noun_id , pred_name = pred_name))
+	ui.output(val)
+
+def processer_ask_all(ui , cmd):
+	noun_id = int(cmd[1])
+	pred_name = (" ".join(cmd[2:])).encode("utf-8")
+	pred_name = str( b64encode(  pred_name ) , encoding = "utf-8")
+	val = ui.req.request("ask_all/{noun_id}/{pred_name}".format(noun_id = noun_id , pred_name = pred_name))
+	ui.output(val)
+
+
 def main():
 	ui = CUI()
 	ui.add_processor("conn" 	  , processer_connect   , "[ip:str , port: int] give ip & port")
 	ui.add_processor("noun_cnt"   , processer_noun_cnt  , "[] ask number of nouns")
-	ui.add_processor("syntax" , processer_ask_syntax  , "[noun_id: int] ask syntax of specific noun")
+	ui.add_processor("syntax" , processer_ask_syntax    , "[noun_id: int] ask syntax of specific noun")
+	ui.add_processor("askval" , processer_ask_val       , "[noun_id: int, pred_name: str] ask a certain value")
+	ui.add_processor("askall" , processer_ask_all       , "[noun_id: int, pred_name: str] ask all values")
 	ui.fake_input("conn 127.0.0.1 7899")
 	ui.fake_input("noun_cnt")
 	ui.fake_input("syntax 346")
+	ui.fake_input("askval 346 test loss")
 	ui.run() 
 
 if __name__ == "__main__":
--- fitterserver/__init__.py
+++ fitterserver/__init__.py
@@ -1,14 +1,26 @@
 from YTools.network.server.server import start_server
-from fitterlog.interface.restore import load_noun_number , load_syntax , load_last
+from fitterlog.interface.restore import load_noun_number , load_syntax , load_last , load_all
 from fitterlog.core.morphology import Noun , Predicate
+from base64 import b64encode , b64decode
 
 def noun_cnt(request):
 	return load_noun_number()
 def ask_syntax(request , noun_id):
 	the_noun = Noun(noun_id)
 	return load_syntax(the_noun)
+def ask_val(request , noun_id , pred_name):
+	the_noun = Noun(noun_id)
+	the_pred = Predicate(str(b64decode(pred_name) , encoding = "utf-8"))
+	return load_last(the_noun , the_pred , False)
+
+def ask_all(request , noun_id , pred_name):
+	the_noun = Noun(noun_id)
+	the_pred = Predicate(str(b64decode(pred_name) , encoding = "utf-8"))
+	return load_all(the_noun , the_pred , False)
 
 start_server(ip = "127.0.0.1" , port = "7899" , responsers = {
 	"noun_cnt" : noun_cnt , 
 	"ask_syntax/<int:noun_id>" : ask_syntax , 
+	"ask_all/<int:noun_id>/<str:pred_name>" : ask_all , 
+	"ask_val/<int:noun_id>/<str:pred_name>" : ask_val , 
 })
